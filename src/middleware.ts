5275ce3e655cb8833e6967e4008b9ed3f5eb8c75
Tomas <tomas@github.com>
2026-03-12 18:39:24 -0300

    Fix middleware to allow debug API

--- src/middleware.ts
+++ src/middleware.ts
@@ -67,14 +67,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     * - uploads folder
-     */
-    '/((?!_next/static|_next/image|favicon.ico|public|uploads|images).*)',
+    '/((?!api/debug|_next/static|_next/image|favicon.ico|public|uploads|images).*)',
   ],
 }
